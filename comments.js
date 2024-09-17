b246b7896df7a6fc3187d9251a92b37b76229fe2
selc3435 <147780872+selc3435@users.noreply.github.com>
2024-09-17 11:13:31 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server
+// Run: node comments.js
+// Open browser: http://localhost:3000/
+
+var http = require('http');
+var fs = require('fs');
+
+// Create server
+http.createServer(function (req, res) {
+    //
